f23348d2f5522a24e1d997c1c29178fefce5cfd1
AriadnaNolasco <ariadna.nolasco@tecsup.edu.pe>
2025-07-03 00:48:06 -0500

    p

--- Front-React/src/services/EmployeeService.jsx
+++ Front-React/src/services/EmployeeService.jsx
@@ -1,7 +1,7 @@
 // src/services/EmployeeService.js
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.local.REACT_APP_API_URL;
 
 const EMPLOYEE_API_BASE_PATH = "/empleados";
 class EmployeeService {
